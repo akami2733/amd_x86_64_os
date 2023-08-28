5149eeba25cf3d5f52399f6440a80337cacef88f
akami2733 <akami2733@gmail.com>
2023-08-28 18:21:26 +0900

    .

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-qemu-system-x86_64.exe -bios C:\Users\akami\data\develop\qemu\OVMF.fd -drive file=fat:rw:C:\Users\akami\data\develop\qemu\os-sim
+qemu-system-x86_64.exe -bios C:\Users\akami\data\develop\amd_x86_64_os\OVMF.fd -drive file=fat:rw:C:\Users\akami\data\develop\amd_x86_64_os\os-sim
